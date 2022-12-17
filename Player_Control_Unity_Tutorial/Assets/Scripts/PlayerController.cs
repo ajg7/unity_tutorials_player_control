d13501fd45f9adc9436adf7e33da1a660fa5e7a2
AJ <ajgebara@gmail.com>
2022-12-17 12:01:04 -0600

    Finished Unity Junior Programmer Tutorial 1

--- Player_Control_Unity_Tutorial/Assets/Scripts/PlayerController.cs
+++ Player_Control_Unity_Tutorial/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] float speed = 20f;
+    [Range(0.0f, 10.0f)]
+    [SerializeField] float turnSpeed = 7f;
+    [SerializeField] float horizontalSpeed;
+    [SerializeField] float forwardSpeed;
     void Update()
     {
-        
+        horizontalSpeed = Input.GetAxis("Horizontal");
+        forwardSpeed = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardSpeed);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalSpeed);
     }
 }
